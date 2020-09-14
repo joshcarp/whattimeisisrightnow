e78e31e8543e7a5ce0e9b6b05b7e876b14984a9e
joshcarp <joshuacarpeggiani@gmail.com>
2020-09-14 20:55:56 +1000

    Print log trace

--- whattimeisitrightnow.go
+++ whattimeisitrightnow.go
@@ -1,10 +1,12 @@
 package whattimeisitrightnow
 
 import (
+	"log"
 	"net/http"
 	"time"
 )
 
 func ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Println(r)
 	w.Write([]byte(time.Now().String()))
 }
